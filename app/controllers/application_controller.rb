6a92c6e986622dd33aa8988ab4e1407ec815eafe
Yasir <yasirch7722@gmail.com>
2017-12-01 23:10:56 +0500

    complete tenant and users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_tenant!
   protect_from_forgery with: :exception
-  before_action :authenticate_tenant!
+ 
   
      ##    milia defines a default max_tenants, invalid_tenant exception handling
      ##    but you can override these if you wish to handle directly
